0bc83b3469cdaf8d0a32528c66726f420e54d3ae
qspid <qspid@LAPTOP-G2U1P6ST>
2024-04-12 08:42:23 +0530

    first commit

---
+++ src/test/java/demo/MavenJenkins.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class MavenJenkins {
+
+	@Test
+	public void mavenJenkins() {
+		Reporter.log("MavenJenkins project working successfully",trues);
+	}
+}
