d41cec0b32ae6ce7adac11d1ecb01f476fbc34f2
qspid <qspid@LAPTOP-G2U1P6ST>
2024-04-12 10:01:13 +0530

    ferrari commit

---
+++ src/test/java/demo/FerrariTest.java
@@ -0,0 +1,11 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class FerrariTest {
+	@Test
+	public void Ferrari() {
+		Reporter.log("Ferrari beast",true);
+	}
+}
