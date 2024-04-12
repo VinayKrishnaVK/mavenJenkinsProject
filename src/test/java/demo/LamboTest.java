c5808fc65c2de50dfa8eb4f9518d7187f18eaca5
qspid <qspid@LAPTOP-G2U1P6ST>
2024-04-12 09:58:14 +0530

    lambo commit

---
+++ src/test/java/demo/LamboTest.java
@@ -0,0 +1,12 @@
+package demo;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class LamboTest {
+
+	@Test
+	public void lambo() {
+		Reporter.log("Lamborgini hurrcan",true);
+	}
+}
